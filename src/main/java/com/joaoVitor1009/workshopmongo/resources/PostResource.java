99c49745aded547c9e68c7b653c4c96227e3a0b5
joaoVitor1009 <joaovitorsg1804@gmail.com>
2026-06-25 14:09:59 -0300

    Created PostService and Resource, findById implemented

---
+++ src/main/java/com/joaoVitor1009/workshopmongo/resources/PostResource.java
@@ -0,0 +1,32 @@
+package com.joaoVitor1009.workshopmongo.resources;
+
+import com.joaoVitor1009.workshopmongo.DTO.UserDTO;
+import com.joaoVitor1009.workshopmongo.domain.Post;
+import com.joaoVitor1009.workshopmongo.domain.User;
+import com.joaoVitor1009.workshopmongo.services.PostService;
+import com.joaoVitor1009.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+
+}
---
+++ src/main/java/com/joaoVitor1009/workshopmongo/services/PostService.java
@@ -0,0 +1,33 @@
+package com.joaoVitor1009.workshopmongo.services;
+
+import com.joaoVitor1009.workshopmongo.DTO.UserDTO;
+import com.joaoVitor1009.workshopmongo.domain.Post;
+import com.joaoVitor1009.workshopmongo.domain.User;
+import com.joaoVitor1009.workshopmongo.repositories.PostRepository;
+import com.joaoVitor1009.workshopmongo.repositories.UserRepository;
+import com.joaoVitor1009.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo ;
+
+    public Post findById(String id){
+        Optional<Post> post = repo.findById(id);
+        Post p = null;
+        if(!post.isEmpty()){
+            p = post.get();
+        }
+        else {
+            throw new ObjectNotFoundException("Post Not Found");
+        }
+        return p;
+    }
+
+}
